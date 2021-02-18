1cec8de544b625e3a3f8504961da9abce0693fbe
humorous-storage-7521 <rmwerner@umich.edu>
2021-02-18 09:30:06 -0600

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,27 @@
 # Implement your procedural solution here!
+def get_factors(num)
+    count = 1
+    arr = []
+    while count <= Math.sqrt(num)
+        if num % count == 0
+            arr<<count
+        end
+        count += 1
+    end
+    return arr
+end
+
+
+def prime_number_for(num)
+    answer = 2
+    prime_count = 0
+    count = 1
+    while prime_count <= num
+        if get_factors(count) == [1]
+            answer = count 
+            prime_count += 1
+        end
+        count += 1
+    end
+    return answer
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,33 @@
 # Implement your object-oriented solution here!
+class Prime
+    
+    def initialize(num)
+        @num = num
+    end
+
+    def get_factors(num)
+        count = 1
+        arr = []
+        while count <= Math.sqrt(num)
+            if num % count == 0
+                arr<<count
+            end
+            count += 1
+        end
+        return arr
+    end
+     
+    def number
+        answer = 2
+        prime_count = 0
+        count = 1
+        while prime_count <= @num
+            if get_factors(count) == [1]
+                answer = count 
+                prime_count += 1
+            end
+            count += 1
+        end
+        return answer
+    end
+end
